1f3a5db76fbe7bb5e8864ef218f421da73730171
Romil Monpara <151631804+romilmonpara@users.noreply.github.com>
2025-03-21 18:15:15 +0530

    Delete Chap.-2 Node.Js directory

--- Chap.-2 Node.Js/02_basic.js
+++
@@ -1,9 +0,0 @@
-var ps=require("fs");
-ps.mkdirSync("Hello"); 
-ps.writeFileSync("Hello/user.txt","Hello"); 
-ps.appendFileSync("Hello/user.txt","\nWorld");
-var data = ps.readFileSync("Hello/user.txt","utf-8");
-ps.renameSync("Hello/user.txt","Hello/user1.txt");
-console.log(data.toString());
-ps.unlinkSync("Hello/user1.txt");
-ps.rmdirSync("Hello");
